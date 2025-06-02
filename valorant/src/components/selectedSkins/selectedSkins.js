6462d6b03e88916a65eae1b8304a7bd63269acad
SKMonarch <christiansanlu@gmail.com>
2025-06-02 12:09:35 +0200

    #4.1 Almacenamiento

--- valorant/src/components/selectedSkins/selectedSkins.js
+++ valorant/src/components/selectedSkins/selectedSkins.js
@@ -1,6 +1,6 @@
 
 const SelectedSkins = ({skinsFiltradas,skinSeleccionada,setSkinSeleccionada,nameForRarezaUuid}) =>{
-  const transacciones = JSON.parse(localStorage.getItem("transacciones")) || [];
+
   const skinDefault = (skin) => {
 
     if (skin.displayName.includes("Standard") || skin.displayName.includes("Meele")){
@@ -8,9 +8,6 @@ const SelectedSkins = ({skinsFiltradas,skinSeleccionada,setSkinSeleccionada,name
     }
     return skin;
     
-    const isSkinInTransactions = (skinUuid) => {
-    return transacciones.some(trans => trans.skinUuid === skinUuid);
-    }
 
 
     
@@ -21,7 +18,6 @@ return <>
     {skinsFiltradas.map((skin, index) => (
       <div key={index} className={`skin-card 
             ${skinSeleccionada?.uuid === skin.uuid ? "selected" : ""}
-            ${isSkinInTransactions(skin.uuid) ? "owned" : ""}
           `} onClick={() => setSkinSeleccionada(skinDefault(skin))} >
         <img src={skin.displayIcon} alt={skin.displayName} />
         <p>{skin.displayName}</p>
